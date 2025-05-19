efe915dc5a6086fa138b64ff5bcf1deb46d5e0b9
NickEngmann <cyengmann@gmail.com>
2025-05-19 11:11:04 -0400

    chore:cleanup repo

--- INSTALL.sh
+++ INSTALL.sh
@@ -149,6 +149,23 @@ else
   echo "X11DisplayOffset 10" | sudo tee -a /etc/ssh/sshd_config
 fi
 
+
+sudo apt install -y python3-pip python3-venv git
+
+mkdir -p ~/github/luxonis
+cd ~/github/luxonis
+git clone https://github.com/luxonis/depthai.git
+cd ~/github/luxonis/depthai
+python3 -m venv virtualenv
+source virtualenv/bin/activate
+pip install -U pip
+python3 install_requirements.py
+sudo apt-get -y install libatlas-base-dev python3-h5py
+echo 'SUBSYSTEM=="usb", ATTRS{idVendor}=="03e7", MODE="0666"' \
+    | sudo tee /etc/udev/rules.d/80-movidius.rules
+sudo udevadm control --reload-rules \
+    && sudo udevadm trigger
+    
 # Restart SSH service
 #sudo systemctl restart sshd
 
--- robot_test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-cd ~/luxopi-ros
-colcon build --packages-select luxo_behaviors
-source install/setup.bash
